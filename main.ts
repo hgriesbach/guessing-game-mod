8a9283d3eb8371643de4d29484e29c7ae86b0dc7
hgriesbach <77631543+hgriesbach@users.noreply.github.com>
2025-01-13 08:35:41 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,9 @@ let secondValue = randint(1, 5)
 game.splash("Answer the question!")
 while (guess != value + secondValue) {
     guess = game.askForNumber("What is " + value + " + " + secondValue)
+    if (guess == value + secondValue) {
+        game.splash("Your a genius")
+    }
 }
 game.splash("Correct!")
 game.over(true)
